26be0a0be89c747b127aba682c5043841b99b38a
esoe <igor_nikulin@mail.ru>
2022-10-20 13:01:48 +0300

    qq

--- files/src/main/java/ru/molokoin/File.java
+++ files/src/main/java/ru/molokoin/File.java
@@ -1,10 +1,16 @@
 package ru.molokoin;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 @Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface File {
     String name() default "noname";
-    byte size() default "0";
+    byte size() default 0;
     Extension extension () default Extension.OTHER;
 }
--- files/src/main/java/ru/molokoin/Storage.java
+++ files/src/main/java/ru/molokoin/Storage.java
@@ -1,6 +1,6 @@
 package ru.molokoin;
 //import annotations
-//@File (name=noname, size=0, extension=Extension.OTHER)
+@File (name="noname", size=0, extension=Extension.OTHER)
 public class Storage {
     //Extension extension;
     public Storage(){
@@ -10,7 +10,6 @@ public class Storage {
 
     }
     
-    //читаем днные аннотации
     public static void main(String[] args) {
         System.out.println("Storage.main()");
         //Storage storage = new Storage();
